299910001191a67a7e7ef33a1c69fdce34106827
Mihail Slavov <mihail_slavov@icloud.com>
2023-09-28 20:50:09 +0300

    Data str imlp

--- L04_LinearDataStructures/src/main/java/Main.java
+++ L04_LinearDataStructures/src/main/java/Main.java
@@ -1,5 +1,21 @@
+import implementations.ArrayList;
+import interfaces.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<Integer> testList = new ArrayList<>();
+
+        testList.add(1);
+        testList.add(5);
+        testList.add(10);
+        testList.add(11);
+        testList.add(15);
+
+        testList.remove(-1);
+
+        for (Integer i : testList) {
+            System.out.println(i);
+        }
 
     }
 }
--- L04_LinearDataStructures/src/main/java/implementations/ArrayList.java
+++ L04_LinearDataStructures/src/main/java/implementations/ArrayList.java
@@ -6,55 +6,179 @@ import java.util.Arrays;
 import java.util.Iterator;
 
 public class ArrayList<E> implements List<E> {
+    public static final int INITIAL_SIZE = 4;
+    private Object[] elements;
+    private int size = 0; //current number of elements
+    private int capacity = INITIAL_SIZE; //current size (колко му е цялостния размер)
+
+    public ArrayList() {
+        this.elements = new Object[INITIAL_SIZE];
+    }
 
     @Override
     public boolean add(E element) {
 
-        return false;
+        if (this.size == this.capacity) {
+            grow();
+        }
+
+        //започваме от първи елемент и след, като масива е празен направо следим индекса с променливата за броя на елементите му, които има в момента в него.
+        // Когато Size==0 нямаме елементи и добавяме на нулева позиция
+        this.elements[this.size] = element; //може направо и тук да го добавим, като: this.elements[this.size++] = element;
+        this.size++;
+
+        return true;
     }
 
     @Override
     public boolean add(int index, E element) {
-        return false;
+        if (!validIndex(index)) {
+            return false;
+        }
+
+        shiftRight(index);
+        this.elements[index] = element;
+        this.size++;
+
+        return true;
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public E get(int index) {
-        return null;
+        validateAndTrow(index);
+        return (E) this.elements[index];
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public E set(int index, E element) {
-        return null;
+
+        validateAndTrow(index);
+
+        Object currentElementInIndexPlace = this.elements[index];
+        this.elements[index] = element;
+        return (E) currentElementInIndexPlace;
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public E remove(int index) {
-        return null;
+
+        validateAndTrow(index);
+
+        Object currentElementInIndexPlace = this.elements[index];
+
+        shiftLeft(index);
+        this.size--;
+
+        shrinkIfNeeded();
+
+        return (E) currentElementInIndexPlace;
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.size;
     }
 
     @Override
     public int indexOf(E element) {
-        return 0;
+        for (int i = 0; i < this.size; i++) {
+            if (this.elements[i].equals(element)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public boolean contains(E element) {
-        return false;
+        return this.indexOf(element) != -1;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return this.size == 0;
     }
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new Iterator<E>() {
+            private int index = 0;
+            @Override
+            public boolean hasNext() {
+                return index < size();
+            }
+
+            @Override
+            public E next() {
+                //вика get от index и после му увеличава стойността
+                return get(index++);
+            }
+        };
     }
+
+    private void grow() {
+
+        this.capacity *= 2;
+        Object[] temp = new Object[this.capacity];
+
+        for (int i = 0; i < this.elements.length; i++) {
+            temp[i] = this.elements[i];
+        }
+        //Копираме си обратно масива във стария
+        this.elements = temp;
+
+    }
+
+    //цепим масива до индекса и местим всичко надясно
+    private void shiftRight(int index) {
+        for (int i = this.size - 1; i >= index; i--) {
+            this.elements[i + 1] = this.elements[i];
+        }
+    }
+
+    private void shiftLeft(int index) {
+        for (int i = index; i < this.size - 1; i++) {
+            this.elements[i] = this.elements[i + 1];
+        }
+    }
+
+    private boolean validIndex(int index) {
+        return index >= 0 && index < this.size;
+    }
+
+    private void validateAndTrow(int index) {
+        if (!validIndex(index)) {
+            throw new IndexOutOfBoundsException("Index " + index + " is not in ArrayList with size " + this.size);
+        }
+    }
+    private void shrinkIfNeeded() {
+        if (this.size > this.capacity / 3) {
+            return;
+        }
+        this.capacity /= 2;
+        this.elements = Arrays.copyOf(this.elements, this.capacity);
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
