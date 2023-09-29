dcd45d37d9fdc35cf4a805ac942124a1f372e536
Mihail Slavov <mihail_slavov@icloud.com>
2023-09-29 18:02:52 +0300

    Nodes impl

--- L04_LinearDataStructures/src/main/java/implementations/SinglyLinkedList.java
+++ L04_LinearDataStructures/src/main/java/implementations/SinglyLinkedList.java
@@ -6,48 +6,142 @@ import java.util.Iterator;
 
 public class SinglyLinkedList<E> implements LinkedList<E> {
 
+    private Node<E> head;
+    private int size;
+
+    private static class Node<E> {
+        private E value;
+        private Node<E> next;
+
+        public Node(E element) {
+            this.value = element;
+            this.next = null;
+        }
+    }
+
+    public SinglyLinkedList() {
+        this.head = null;
+        this.size = 0;
+    }
+
     @Override
     public void addFirst(E element) {
 
+        Node<E> nextElement = new Node<>(element);
+        nextElement.next = this.head;
+        this.head = nextElement;
+
+        this.size++;
     }
 
     @Override
     public void addLast(E element) {
+        Node<E> lastElement = new Node<>(element); // елемента, който ще добавяме
 
+        if (this.isEmpty()) { //ако опашката е празна
+            this.head = lastElement;
+            this.size++;
+            return;
+        }
+
+        Node<E> current = this.head; //взимаме първият елемент
+
+        while (current.next != null) {
+            current = current.next; //текущият отива на следващият елемент в редицата
+        }
+
+        current.next = lastElement;
+
+        this.size++;
     }
 
     @Override
     public E removeFirst() {
-        return null;
+
+        ensureNonEmpty();
+
+        E value = this.head.value;
+        this.head = this.head.next; //сегашният head започва да сочи, към не сегашният head следващият елемент;
+
+        this.size--;
+
+        return value;
     }
+
 
     @Override
     public E removeLast() {
-        return null;
+        ensureNonEmpty();
+
+        if (this.size == 1) {
+            E value = this.head.value;
+            this.head = null;
+            this.size--;
+            return value;
+        }
+
+        Node<E> preLast = this.head; // Предпоследният за да може, като му направим .next на null, все едно да го изтрием последният
+        Node<E> toRemove = this.head.next; //започваме от първият
+
+        while (toRemove.next != null) {
+            preLast = toRemove;
+            toRemove = toRemove.next;
+        }
+
+        preLast.next = null;
+        this.size--;
+        return toRemove.value;
     }
 
     @Override
     public E getFirst() {
-        return null;
+        ensureNonEmpty();
+        return this.head.value;
     }
 
     @Override
     public E getLast() {
-        return null;
+        ensureNonEmpty();
+        Node<E> current = this.head;
+
+        while (current.next != null) {
+            current = current.next;
+        }
+        return current.value;
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return this.size == 0;
     }
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new Iterator<E>() {
+            private Node<E> current = head;
+
+            @Override
+            public boolean hasNext() {
+                return current != null;
+            }
+
+            @Override
+            public E next() {
+                E value = current.value;
+                current = current.next;
+                return value;
+            }
+        };
+    }
+
+    private void ensureNonEmpty() {
+        if (this.isEmpty()) {
+            throw new IllegalStateException();
+        }
     }
 }
--- L05_ExerciseLinearDataStructures/src/main/java/implementations/Queue.java
+++ L05_ExerciseLinearDataStructures/src/main/java/implementations/Queue.java
@@ -5,61 +5,55 @@ import interfaces.AbstractQueue;
 import java.util.Iterator;
 
 public class Queue<E> implements AbstractQueue<E> {
-
     private Node<E> head;
     private int size;
 
     private static class Node<E> {
-        private E value;
+        private E element;
         private Node<E> next;
 
-        Node(E element) {
-            this.value = element;
+        private Node(E element) {
+            this.element = element;
         }
     }
 
     public Queue() {
-        this.head = null;
-        size = 0;
     }
 
     @Override
     public void offer(E element) {
-
-        Node<E> toInsert = new Node<>(element);
-
-        if (this.isEmpty()) {
-            this.head = toInsert;
-            this.size++;
-            return;
+        Node<E> newNode = new Node<>(element);
+        if (this.head == null) {
+            this.head = newNode;
+        } else {
+            Node<E> current = this.head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
         }
-
-        Node<E> current = this.head;
-
-        while (current.next != null) {
-            current = current.next;
-        }
-
-        current.next = toInsert;
         this.size++;
     }
 
     @Override
     public E poll() {
         ensureNonEmpty();
-
-        Node<E> first = this.head;
-        this.head = first.next;
-
+        E element = this.head.element;
+        if (this.size == 1) {
+            this.head = null;
+        } else {
+            Node<E> next = this.head.next;
+            this.head.next = null;
+            this.head = next;
+        }
         this.size--;
-        return first.value;
+        return element;
     }
-
 
     @Override
     public E peek() {
         ensureNonEmpty();
-        return this.head.value;
+        return this.head.element;
     }
 
     @Override
@@ -79,23 +73,21 @@ public class Queue<E> implements AbstractQueue<E> {
 
             @Override
             public boolean hasNext() {
-                return current != null;
+                return this.current != null;
             }
 
             @Override
             public E next() {
-                E value = current.value;
-
-                current = current.next;
-
-                return value;
+                E element = this.current.element;
+                this.current = this.current.next;
+                return element;
             }
         };
     }
 
     private void ensureNonEmpty() {
-        if (this.isEmpty()) {
-            throw new IllegalStateException();
+        if (this.size == 0) {
+            throw new IllegalStateException("Illegal operation on empty stack");
         }
     }
 }
