31fbcc30cef07c22035aea518c4b9c9a779fb379
Mihail Slavov <mihail_slavov@icloud.com>
2023-10-11 22:36:24 +0300

    nodes

--- Exams/Exam/src/main/java/craftsmanLab/core/CraftsmanLabImpl.java
+++ Exams/Exam/src/main/java/craftsmanLab/core/CraftsmanLabImpl.java
@@ -8,33 +8,36 @@ import java.util.stream.Collectors;
 
 public class CraftsmanLabImpl implements CraftsmanLab {
 
-    //FIXME: needs more work
-    Map<String, ApartmentRenovation> apartments;
-    Map<String, Craftsman> craftsmans;
-    Map<ApartmentRenovation, Craftsman> apartmentRenovationCraftsman;
+    private final Map<String, ApartmentRenovation> apartments;
+    private final Map<String, Craftsman> craftsmen;
+    private final PriorityQueue<Craftsman> craftsmenByProfit;
+    private final Map<String, Craftsman> apartmentRenovator;
 
 
     public CraftsmanLabImpl() {
-        apartments = new LinkedHashMap<>();
-        craftsmans = new HashMap<>();
-        apartmentRenovationCraftsman = new LinkedHashMap<>();
+        this.apartments = new LinkedHashMap<>();
+        this.craftsmen = new HashMap<>();
+        this.craftsmenByProfit = new PriorityQueue<>(Comparator.comparingDouble(c -> c.totalEarnings));
+        this.apartmentRenovator = new HashMap<>();
     }
 
     @Override
     public void addApartment(ApartmentRenovation job) {
-        if (apartments.containsKey(job.address)) {
+        if (exists(job)) {
             throw new IllegalArgumentException();
         }
+
         apartments.put(job.address, job);
-        apartmentRenovationCraftsman.put(job, null);
     }
 
     @Override
     public void addCraftsman(Craftsman craftsman) {
-        if (craftsmans.containsKey(craftsman.name)) {
+        if (exists(craftsman)) {
             throw new IllegalArgumentException();
         }
-        craftsmans.put(craftsman.name, craftsman);
+
+        craftsmen.put(craftsman.name, craftsman);
+        craftsmenByProfit.add(craftsman);
     }
 
     @Override
@@ -44,118 +47,85 @@ public class CraftsmanLabImpl implements CraftsmanLab {
 
     @Override
     public boolean exists(Craftsman craftsman) {
-        return craftsmans.containsKey(craftsman.name);
+        return craftsmen.containsKey(craftsman.name);
     }
 
     @Override
     public void removeCraftsman(Craftsman craftsman) {
-        if (!craftsmans.containsKey(craftsman.name)) {
+        if (!exists(craftsman)) {
             throw new IllegalArgumentException();
         }
 
-        for (Craftsman current : apartmentRenovationCraftsman.values()) {
-            if (current.equals(craftsman)) {
-                throw new IllegalArgumentException();
-            }
+        if (apartmentRenovator.containsValue(craftsman)) {
+            throw new IllegalArgumentException();
         }
 
-        craftsmans.remove(craftsman.name);
+        craftsmenByProfit.remove(craftsman);
+        craftsmen.remove(craftsman.name);
     }
 
     @Override
     public Collection<Craftsman> getAllCraftsmen() {
-        return craftsmans.values();
+        return craftsmen.values();
     }
 
     @Override
     public void assignRenovations() {
-        List<Craftsman> craftsmanByMoney = craftsmans.values()
-                .stream()
-                .sorted(Comparator.comparing(Craftsman::getTotalEarnings))
-                .collect(Collectors.toList());
-
-        for (ApartmentRenovation currentAp : apartmentRenovationCraftsman.keySet()) {
-            if (apartmentRenovationCraftsman.get(currentAp) == null) {
-                int index = 0;
-                while (index < craftsmanByMoney.size()) {
-                    Craftsman craftsman = craftsmanByMoney.remove(0);
-                    craftsman.totalEarnings += currentAp.workHoursNeeded * craftsman.getHourlyRate();
-                    apartmentRenovationCraftsman.put(currentAp, craftsman);
-                    index++;
-                }
+        for (ApartmentRenovation job : apartments.values()) {
+            if (apartmentRenovator.containsKey(job.address)) {
+                continue;
             }
+
+            Craftsman doingRepair = craftsmenByProfit.poll();
+
+            doingRepair.totalEarnings += job.workHoursNeeded * doingRepair.hourlyRate;
+
+            craftsmenByProfit.offer(doingRepair);
+            apartmentRenovator.put(job.address, doingRepair);
         }
     }
 
-    @Override
-    public Craftsman getContractor(ApartmentRenovation job) {
-        if (apartmentRenovationCraftsman.get(job) == null) {
-            throw new IllegalArgumentException();
+        @Override
+        public Craftsman getContractor(ApartmentRenovation job) {
+            if (!apartmentRenovator.containsKey(job.address)) {
+                throw new IllegalArgumentException();
+            }
+
+            return apartmentRenovator.get(job.address);
         }
-        return apartmentRenovationCraftsman.get(job);
-    }
 
     @Override
     public Craftsman getLeastProfitable() {
-////        return craftsmans.values()
-////                .stream()
-////                .sorted(Comparator.comparing(Craftsman::getTotalEarnings).reverced())
-////                .collect(Collectors.toList()).get(0);
-//        double least = -10;
-//        String resultName = null;
-//        for (Craftsman current : craftsmans.values()) {
-//            if (current.totalEarnings>least){
-//                least = current.totalEarnings;
-//                resultName = current.name;
-//                break; // nope
-//            }
-//        }
-//        return craftsmans.get(resultName);
-
-        double least = Double.MAX_VALUE;
-        String resultName = null;
-
-        for (Craftsman current : craftsmans.values()) {
-            if (current.totalEarnings < least) {
-                least = current.totalEarnings;
-                resultName = current.name;
-                break; //
-            }
+        if (craftsmenByProfit.isEmpty()) {
+            throw new IllegalArgumentException();
         }
 
-        return craftsmans.get(resultName);
+        return craftsmenByProfit.peek();
     }
 
     @Override
     public Collection<ApartmentRenovation> getApartmentsByRenovationCost() {
 
-        return apartmentRenovationCraftsman.keySet()
-                .stream()
-                .sorted((a1, a2) -> {
+        return apartments.values().stream()
+                .sorted((l, r) -> {
+                    Craftsman leftCraftsman = apartmentRenovator.get(l.address);
+                    Craftsman rightCraftsman = apartmentRenovator.get(r.address);
 
-                    double first;
-                    double second;
+                    double leftCost = leftCraftsman == null ?
+                            l.workHoursNeeded : leftCraftsman.hourlyRate * l.workHoursNeeded;
+                    double rightCost = rightCraftsman == null ?
+                            r.workHoursNeeded : rightCraftsman.hourlyRate * r.workHoursNeeded;
 
-                    if (apartmentRenovationCraftsman.get(a1) == null) {
-                        first = a1.workHoursNeeded;
-                    } else {
-                        first = apartmentRenovationCraftsman.get(a1).getHourlyRate() * a1.workHoursNeeded;
-                    }
-
-                    if (apartmentRenovationCraftsman.get(a2) == null) {
-                        second = a2.workHoursNeeded;
-                    } else {
-                        second = apartmentRenovationCraftsman.get(a2).getHourlyRate() * a2.workHoursNeeded;
-                    }
-                    return Double.compare(second, first);
-                }).collect(Collectors.toList());
+                    return Double.compare(rightCost, leftCost);
+                })
+                .collect(Collectors.toList());
     }
 
     @Override
     public Collection<ApartmentRenovation> getMostUrgentRenovations(int limit) {
         return apartments.values()
                 .stream()
-                .sorted(Comparator.comparing(ApartmentRenovation::getDeadline))
+                .sorted(Comparator.comparing(c -> c.deadline))
                 .limit(limit)
                 .collect(Collectors.toList());
     }
